a4f95aad2f548faf96c0895aa48ef1d3ee02e7bc
Fatime Useini <112587115+FatimeUseini@users.noreply.github.com>
2022-09-06 22:04:31 +0000

    simple.py

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,17 @@
+print(10 * 5)
+print(10**2)
+print(15 / 10)
+print(15 // 10)
+print(-15 // 10)
+print(15 % 10)
+print(10 % 15)
+print(10 % 10)
+print(0 % 10)
+print(10 / 15)
+#It prints repeating sixes when the actual answer is neverending.
+rate = input("What is the current exchange from euro to dollar?:")
+rate = float(rate)
+amount = int(input("What is the amount of currency for exchange?:"))
+total = amount * rate
+result = total - 3
+print(result)
