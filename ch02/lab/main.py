b7e2cc16c86858eae02a96be91575ad6f56bfedb
Fatime Useini <112587115+FatimeUseini@users.noreply.github.com>
2022-09-09 19:24:24 +0000

    main.py

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -5,6 +5,13 @@ classes = 5
 tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
-
-
+classes_per_week = 3
+print("Classes per week:", classes_per_week)
+cost_per_class= (cost_per_week/classes_per_week)
+print("Hello!, your cost per class:",cost_per_class)
 #Part B
+import random
+number_list= (3,6,9,12,15)
+print("Number List:", number_list)
+chosen_number= random.choice(number_list)
+print("Chosen Number:", chosen_number)
