dbbf8a70b1480893d3818693b957528e88148ec8
Fatime Useini <112587115+FatimeUseini@users.noreply.github.com>
2022-09-19 20:55:03 +0000

    guess.py

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -2,7 +2,7 @@ import random
 number_range= (1,2,3,4,5,6,7,8,9,10)
 print("Number List:", number_range)
 chosen_number= random.choice(number_range)
-while True: 
+for i in range(3): 
   num=int(input("Enter a number between 1 and 10?:"))
   if chosen_number==num:
     print("correct!")
