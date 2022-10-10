da4a413cc5246fb56f350bff173f2da47b97c10e
Fatime Useini <112587115+FatimeUseini@users.noreply.github.com>
2022-10-10 22:00:23 +0000

    patterns.py

--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -1,5 +1,4 @@
 star="*"
-star1="*"
 star_pyramid=int(input("how many rows?:"))
   
 for i in range(star_pyramid):
@@ -10,5 +9,5 @@ for i in range(star_pyramid):
 r_star_pyramid=int(input("how many rows?:"))
 for i in range(r_star_pyramid):
   i= star_pyramid-i
-  print(star1*i)
+  print(star*i)
   
