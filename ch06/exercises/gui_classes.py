de928532b21541a4ba632bf31a0cb8e558a83e88
Fatime Useini <112587115+FatimeUseini@users.noreply.github.com>
2022-10-24 22:01:25 +0000

    gui

--- ch06/exercises/gui_classes.py
+++ ch06/exercises/gui_classes.py
@@ -1,11 +1,19 @@
 class Player:
-  def __init__(self,pnum):
-    """
+  def __init__(self):
+    self.player.num = 1
+    self.color = (250,0,0)
+    self.position = (0,10)
+
+class Block:
+  def __init__(self):
+    self.block_num=4
+    self.position= (50,50)
+    self.is_small= True
     
-    
-    
-    """
-    self.player.num = pnum
-    self.lives = 3
-    self.is_large = False 
+class Mushroom:
+  def __init__(self):
+    self.position= (10,100)
+    self.forward= 50
+    self.turn= 180
+    self.speed= 5
     
