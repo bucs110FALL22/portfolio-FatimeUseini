1caa38c26d4840a49754ae79d8e036f5c8f8dc87
Fatime Useini <112587115+FatimeUseini@users.noreply.github.com>
2022-10-28 22:58:35 +0000

    exam.py

--- ch06/exam/exam.py
+++ ch06/exam/exam.py
@@ -2,31 +2,48 @@ import turtle
 my_turtle = turtle.Turtle()
 window = turtle.Screen()
 window.colormode(255)
+white= (255,255,255)
+blue1= (11,20,50)
+blue2= (137,207,240)
+turtle.Screen().bgcolor(blue2)
+my_turtle.color(white)
 
+radius= 4
+def heart():
+  my_turtle.color(blue1)
+  my_turtle.left(45)
+  my_turtle.forward(10)
+  my_turtle.backward(10)
+  my_turtle.right(90)
+  my_turtle.forward(10)
+  my_turtle.circle(radius,220)
+  my_turtle.left(180)
+  my_turtle.circle(radius,220)
+  my_turtle.left(45)
 
-def snowflake(size, x_pos, y_pos,l):
-  angle= size*2
+def angle(num_branch):
+  angle=(360/num_branch)
+  return angle
+  return num_branch
+
+def heart_snowflake(size, x_pos, y_pos,angle, num_branch):
+  my_turtle.pensize(3)
   my_turtle.penup()
   my_turtle.goto(x_pos,y_pos)
   my_turtle.pendown()
-  blue= (137, 207, 240)
-  my_turtle.color(blue)
-  my_turtle.pensize(5)
-  r= int(360/angle)
-  for i in range (r):
+  for i in range (num_branch-1):
     my_turtle.forward(size)
-    my_turtle.left(45)
-    my_turtle.forward(10)
-    my_turtle.backward(10)
-    my_turtle.right(90)
-    my_turtle.forward(10)
-    my_turtle.backward(10)
-    my_turtle.left(l)
-    my_turtle.backward(size)
+    heart()
+    my_turtle.color(white)
+    my_turtle.penup()
+    my_turtle.goto(x_pos,y_pos)
+    my_turtle.pendown()
     my_turtle.left(angle)
-  
-snowflake(25,0,0,45)
-snowflake(20,70,10,45)
+        
+def main():
+  heart_snowflake(25,0,10,angle(8),8)
+main()
+
 window= turtle.Screen()
 window.exitonclick()    
 
