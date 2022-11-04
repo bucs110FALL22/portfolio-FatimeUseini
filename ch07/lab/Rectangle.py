ae0a8881caca3f62022c8ec076e1fdece1ac5fc3
Fatime Useini <112587115+FatimeUseini@users.noreply.github.com>
2022-11-04 21:27:59 +0000

    lab 7

--- ch07/lab/Rectangle.py
+++ ch07/lab/Rectangle.py
@@ -12,17 +12,9 @@ class Rectangle:
       self.width=0
     if self.height<0:
       self.height=0
+      
   def __str__(self): 
     return str.format("(x:{},y:{}) height:{} width:{}".format(self.x, self.y, self.height, self.width,))
 
 
-r = Rectangle(10, 10, 10, 10)
-assert((r.x, r.y, r.height, r.width) == (10,10,10,10))
-r = Rectangle(-1, 1, 1, 1)
-assert((r.x, r.y, r.height, r.width) == (0,1,1,1))
-r = Rectangle(1, -1, 1, 1)
-assert((r.x, r.y, r.height, r.width) == (1,0,1,1))
-r = Rectangle(1, 1, -1, 1)
-assert((r.x, r.y, r.height, r.width) == (1,1,0,1))
-r = Rectangle(1, 1, 1, -1)
-assert((r.x, r.y, r.height, r.width) == (1,1,1,0))
+    
